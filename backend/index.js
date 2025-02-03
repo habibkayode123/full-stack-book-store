153cabc5585a0556e3b9d6b8d37cebfbf599d32a
Habib Popoola <habib@Habibs-MacBook-Pro.local>
2025-02-03 10:22:45 +0100

    working changes

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,47 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const uri =
+	"mongodb+srv://habibkayodenew:Chd83ZmCjakx51ad@cluster0.peeye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
-require('dotenv').config()
+const port = process.env.PORT || 5001;
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
-    credentials: true
-}))
+app.use(
+	cors({
+		origin: [
+			"http://localhost:5174",
+			"https://book-app-frontend-tau.vercel.app"
+		],
+		credentials: true
+	})
+);
 
 // routes
-const bookRoutes = require('./src/books/book.route');
-const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
-const adminRoutes = require("./src/stats/admin.stats")
+const bookRoutes = require("./src/books/book.route");
+const orderRoutes = require("./src/orders/order.route");
+const userRoutes = require("./src/users/user.route");
+const adminRoutes = require("./src/stats/admin.stats");
 
-app.use("/api/books", bookRoutes)
-app.use("/api/orders", orderRoutes)
-app.use("/api/auth", userRoutes)
-app.use("/api/admin", adminRoutes)
+app.use("/api/books", bookRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/admin", adminRoutes);
 
 async function main() {
-  await mongoose.connect(process.env.DB_URL);
-  app.use("/", (req, res) => {
-    res.send("Book Store Server is running!");
-  });
+	await mongoose.connect(uri);
+	app.use("/", (req, res) => {
+		res.send("Book Store Server is running!");
+	});
 }
 
-main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
+main()
+	.then(() => console.log("Mongodb connect successfully!"))
+	.catch((err) => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -1,19 +1,45 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_Auth_Domain,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+//   appId: import.meta.env.VITE_APPID
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const auth =  getAuth(app);
+
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_Auth_Domain,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyCxDBb1mdgF3h2sBQIRHyiLR_yxgVKLGg4",
+  authDomain: "tutor-24d1c.firebaseapp.com",
+  databaseURL: "https://tutor-24d1c.firebaseio.com",
+  projectId: "tutor-24d1c",
+  storageBucket: "tutor-24d1c.firebasestorage.app",
+  messagingSenderId: "871032439224",
+  appId: "1:871032439224:web:1139634f8235bf57c54f4d",
+  measurementId: "G-TMZL2796RQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth =  getAuth(app);
+const analytics = getAnalytics(app);
